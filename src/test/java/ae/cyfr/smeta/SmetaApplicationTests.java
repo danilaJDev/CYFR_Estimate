74508654a82c4310a69f4dce0a7966e1d1fc5476
danilaJDev <danik.rebkovets20@gmail.com>
2025-07-21 11:56:36 +0300

    1

---
+++ src/test/java/ae/cyfr/smeta/SmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package ae.cyfr.smeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
