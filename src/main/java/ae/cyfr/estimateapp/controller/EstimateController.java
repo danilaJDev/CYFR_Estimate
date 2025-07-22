e6e73aa4212de97978548813ec65c16747b54980
danilaJDev <danik.rebkovets20@gmail.com>
2025-07-23 02:28:27 +0300

    16 (ОК)

--- src/main/java/ae/cyfr/estimateapp/controller/EstimateController.java
+++ src/main/java/ae/cyfr/estimateapp/controller/EstimateController.java
@@ -1,9 +1,14 @@
 package ae.cyfr.estimateapp.controller;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import ae.cyfr.estimateapp.model.Estimate;
+import ae.cyfr.estimateapp.model.Work;
 import ae.cyfr.estimateapp.service.EstimateService;
 import ae.cyfr.estimateapp.service.WorkService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,16 +57,24 @@ public class EstimateController {
             return "redirect:/estimate";
         }
 
+        List<Work> works = workService.getWorksByIds(selectedWorkIds);
+
+        Map<Long, Work> workMap = works.stream()
+                .collect(Collectors.toMap(Work::getId, w -> w));
+
         int actualIndex = 0;
         for (int i = 0; i < workIds.size(); i++) {
             Long currentWorkId = workIds.get(i);
             if (selectedWorkIds.contains(currentWorkId)) {
-                Estimate item = new Estimate();
-                item.setWork(workService.getWorkById(currentWorkId));
-                item.setQuantity(quantities.get(actualIndex));
-                item.setCoefficient(coefficients.get(actualIndex));
-                item.setTotalCost(item.getQuantity() * item.getCoefficient() * item.getWork().getClientPrice());
-                estimates.add(item);
+                Work work = workMap.get(currentWorkId);
+                if (work != null) {
+                    Estimate item = new Estimate();
+                    item.setWork(work);
+                    item.setQuantity(quantities.get(actualIndex));
+                    item.setCoefficient(coefficients.get(actualIndex));
+                    item.setTotalCost(item.getQuantity() * item.getCoefficient() * work.getClientPrice());
+                    estimates.add(item);
+                }
                 actualIndex++;
             }
         }
@@ -73,9 +86,14 @@ public class EstimateController {
     public ResponseEntity<byte[]> exportEstimate(@ModelAttribute("estimates") List<Estimate> estimates) throws IOException {
         byte[] excelData = estimateService.createEstimateExcel(estimates);
 
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy__HH.mm");
+        String timestamp = now.format(formatter);
+        String filename = "Estimate__" + timestamp + ".xlsx";
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        headers.setContentDispositionFormData("attachment", "estimate.xlsx");
+        headers.setContentDispositionFormData("attachment", filename);
 
         return ResponseEntity.ok()
                 .headers(headers)
